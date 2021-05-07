ab3e4b8c268b818591b4ee12f08b9a966f6066d6
Jean-Frederic Fortier <jeanfrederic.fortier@shopify.com>
2021-05-07 14:21:47 -0400

    Bring back argo content in SRC

---
+++ src/argo/Context.tsx
@@ -0,0 +1,46 @@
+import React, {useMemo} from 'react';
+
+import '../style.css';
+import {
+  AppContext,
+  AppContextProps,
+  Theme,
+  createTheme,
+  UiTheme,
+} from '../components';
+
+interface Props extends Omit<AppContextProps, 'translate'> {
+  theme?: UiTheme;
+  translate?: AppContextProps['translate'];
+}
+
+const defaultTranslate: AppContextProps['translate'] = (key) => {
+  switch (key) {
+    case 'processing':
+      return 'Processing';
+    case 'submit':
+      return 'Submit';
+    case 'close':
+      return 'Close';
+    default:
+      return '';
+  }
+};
+
+export function Context({
+  children,
+  linkComponent,
+  translate: explicitTranslate,
+  theme: explicitTheme,
+}: Props) {
+  const theme = useMemo(() => explicitTheme ?? createTheme(), [explicitTheme]);
+  const translate = useMemo(() => explicitTranslate ?? defaultTranslate, [
+    explicitTranslate,
+  ]);
+
+  return (
+    <AppContext translate={translate} linkComponent={linkComponent}>
+      <Theme theme={theme}>{children}</Theme>
+    </AppContext>
+  );
+}
